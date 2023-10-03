6efe36c806e98f498d376475e1df3c7162cca1a1
jpg-collab <142198842+jpg-collab@users.noreply.github.com>
2023-10-03 13:24:04 +0000

    cloudflare

---
+++ run.sh
@@ -0,0 +1,15 @@
+cat <<EOL > /home/codespace/.config/imgurbash2/credentials.conf
+CLIENT_ID='5b4a8d5063c5a70'
+CLIENT_SECRET='b5a7f7ecaa6ee0a2afde8f1817aef4abe186b9bf'
+ACCESS_TOKEN='8925ed0f84d1a894822f6dd2df9ac8818ae025a7'
+REFRESH_TOKEN='96ea0035a7ec9d341c9fb66424d931baf5c6320c'
+TOKEN_EXPIRE_TIME='2011698986'
+EOL
+
+
+for folder in */; do
+  folder_name="${folder%/}"
+  for file in "$folder"/*; do
+    ./imgurbash2 -a "rWzTyF0" "$file"
+  done
+done
