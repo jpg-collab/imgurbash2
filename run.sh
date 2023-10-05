81abeef0bd7192ffd12170bcae23e8608f5dd304
jpg-collab <142198842+jpg-collab@users.noreply.github.com>
2023-10-05 12:24:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,10 @@
-cat <<EOL > ./credentials.conf
-CLIENT_ID='5b4a8d5063c5a70'
-CLIENT_SECRET='b5a7f7ecaa6ee0a2afde8f1817aef4abe186b9bf'
-ACCESS_TOKEN='8925ed0f84d1a894822f6dd2df9ac8818ae025a7'
-REFRESH_TOKEN='96ea0035a7ec9d341c9fb66424d931baf5c6320c'
-TOKEN_EXPIRE_TIME='2011698986'
-EOL
+album = 913
+imgur_id = l6SNxu5
 
-git clone https://gitlab.com/ntrungcn/923.git
+git clone "https://gitlab.com/ntrungcn/$album.git"
 
-for folder in */; do
-  folder_name="${folder%/}"
-  for file in "$folder"/*; do
-    ./imgurbash2 -a "rWzTyF0" "$file"
-  done
+cd $album
+
+for file in *; do
+  ./imgurbash2 -l -a "$imgur_id" "$file"
 done
