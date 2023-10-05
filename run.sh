69d71d0f035af4a2ecb5c843c80fe188a21d70c9
jpg-collab <142198842+jpg-collab@users.noreply.github.com>
2023-10-05 13:01:20 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-album="913"
-imgur_id="l6SNxu5"
+album="5478"
+imgur_id="lKR8w0c"
 
 git clone "https://gitlab.com/ntrungcn/$album.git"
 
@@ -11,8 +11,16 @@ ls | head -n 50 | xargs rm
 
 ls -a
 
+result_links=()  # Tạo một mảng để lưu trữ các link kết quả
+
 for file in *; do
-  ./imgurbash2 -l -a "$imgur_id" "$file"
+  result=$(./imgurbash2 -l -a "$imgur_id" "$file")  # Chạy lệnh và lưu kết quả vào biến result
+  result_links+=("$result")  # Thêm kết quả vào mảng
+done
+
+# In ra các link kết quả và ghi vào tệp
+for link in "${result_links[@]}"; do
+  echo "$link" >> /opt/buildhome/repo/index.html
 done
 
 cd ..
