7efcd59cb4c359308a4c9081c205078de38d2ade
jpg-collab <142198842+jpg-collab@users.noreply.github.com>
2023-10-05 15:18:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-album="913"
-imgur_id="l6SNxu5"
+album="366"
+imgur_id="lLOApvK"
 
 git clone "https://gitlab.com/ntrungcn/$album.git"
 
@@ -7,14 +7,10 @@ cp "imgurbash2" "./$album"
 
 cd $album
 
-# ls | head -n 50 | xargs rm
-
 for file in *; do
-  ./imgurbash2 -l -a "$imgur_id" "$file" | grep -oP 'https://i.imgur\.com/\S+' >> index.html
+  ./imgurbash2 -l -a "$imgur_id" "$file"
 done
 
 cd ..
-
-mv "./$album/index.html" ./
 
 find . -type f -size +26M -exec rm {} \;
